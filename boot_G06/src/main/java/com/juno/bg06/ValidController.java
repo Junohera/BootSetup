5e23dc837693b576d67ab1f88b7d72e68d7aa430
junoEzenAnotherPC <wnsgh1110@naver.com>
2021-01-27 17:06:51 +0900

    contentValidator

---
+++ boot_G06/src/main/java/com/juno/bg06/ValidController.java
@@ -0,0 +1,33 @@
+package com.juno.bg06;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ValidController {
+
+    @RequestMapping(value = "/")
+    public String insert1() {
+        return "createPage";
+    }
+
+    @RequestMapping(value = "/create")
+    public String create(Content c, BindingResult result) {
+        /**
+            BindingResult : key, value로 구성된 오류내용 저장클래스
+            위 클래스 형태의 레퍼런스 변수를 선언하고, 시작
+        */
+        System.out.println("c #########################################");
+        System.out.println(c);
+
+        ContentValidator validator = new ContentValidator();
+        validator.validate(c, result);
+
+        if (result.hasError()) {
+            return "createPage";
+        } else {
+            return "createDonePage";
+        }
+    }
+}
