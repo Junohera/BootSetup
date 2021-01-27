769924bda1296beb2afc03b4f99601693b715c4c
junoEzenAnotherPC <wnsgh1110@naver.com>
2021-01-27 16:32:10 +0900

    bg05 init

---
+++ boot_G05/src/main/java/com/juno/bg05/BootG05Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG05Application.class, args);
+	}
+
+}
---
+++ boot_G05/src/test/java/com/juno/bg05/BootG05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.bg05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootG05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
