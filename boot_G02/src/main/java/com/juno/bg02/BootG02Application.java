d234743722359bd52a1e3c5c7ace8ce881008b25
junoEzenAnotherPC <wnsgh1110@naver.com>
2021-01-27 13:58:51 +0900

    bg02

---
+++ boot_G02/src/main/java/com/juno/bg02/BootG02Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG02Application.class, args);
+	}
+
+}
