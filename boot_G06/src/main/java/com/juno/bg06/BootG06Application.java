1c6b113a91b50ae5dcebd2793373788a2b9dd20a
junoEzenAnotherPC <wnsgh1110@naver.com>
2021-01-27 16:45:46 +0900

    06 init

---
+++ boot_G06/src/main/java/com/juno/bg06/BootG06Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG06Application.class, args);
+	}
+
+}
---
+++ boot_G06/src/test/java/com/juno/bg06/BootG06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.bg06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootG06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
