7a9c839e0adb8837924c8dcd0c7ef88afe4ab9c8
junoEzenAnotherPC <wnsgh1110@naver.com>
2021-01-27 14:17:19 +0900

    @ResponseBody 사용

---
+++ boot_G02/src/main/java/com/juno/bg02/JspController.java
@@ -0,0 +1,18 @@
+package com.juno.bg02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JspController {
+
+    @RequestMapping(value = "/")
+    public @ResponseBody String root() {
+    	
+        return "jsp in gradle";
+        // @RequestMapping 값이 "/"이고 리턴값이 "main"이라면
+        // htp://localhost:8070/의 결과는 main.jsp이겠지만 (별도 경로 설정 및 폴더 생성 필요)
+        // 함수 이름에 @ResponseBody가 있으면 리턴되는 문자열이 웹 브라우저에 직접 쓰여지게됩니다.
+    }
+}
