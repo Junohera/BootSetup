688ae13fde76dbf77a39266b43b59b09b74f180f
junoEzenAnotherPC <wnsgh1110@naver.com>
2021-01-27 14:40:24 +0900

    bg03 init

---
+++ boot_G03/src/main/java/com/juno/bg03/BootG03Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG03Application.class, args);
+	}
+
+}
---
+++ boot_G03/src/test/java/com/juno/bg03/BootG03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.bg03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootG03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
