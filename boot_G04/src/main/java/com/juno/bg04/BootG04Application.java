b712e5c123ee6e66901d8cb3744e31b5e47e06e9
junoEzenAnotherPC <wnsgh1110@naver.com>
2021-01-27 15:24:25 +0900

    bg04 init

---
+++ boot_G04/src/main/java/com/juno/bg04/BootG04Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG04Application.class, args);
+	}
+
+}
---
+++ boot_G04/src/test/java/com/juno/bg04/BootG04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.bg04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootG04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
