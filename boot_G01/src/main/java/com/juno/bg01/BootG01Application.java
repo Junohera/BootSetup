506d48318496cab8e6167966f29f7d270571dded
junoEzenAnotherPC <wnsgh1110@naver.com>
2021-01-27 13:37:13 +0900

    boot init by spring boot starter project

---
+++ boot_G01/src/main/java/com/juno/bg01/BootG01Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG01Application.class, args);
+	}
+
+}
