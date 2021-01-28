8b7a61f972bdb6baf1e867769b78e94c47d90268
Juno <juno@Juno-MacBookPro.local>
2021-01-28 10:52:00 +0900

    bg09 init

---
+++ boot_G09/src/main/java/com/juno/bg09/BootG09Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG09Application.class, args);
+	}
+
+}
---
+++ boot_G09/src/test/java/com/juno/bg09/BootG09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.bg09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootG09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
