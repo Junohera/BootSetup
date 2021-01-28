5fdaab1b09e2dc91f2f08e0372df4c2f28cc08a9
Juno <juno@Juno-MacBookPro.local>
2021-01-28 14:18:00 +0900

    bg11 init

---
+++ boot_G11/src/main/java/com/juno/bg11/BootG11Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG11Application.class, args);
+	}
+
+}
