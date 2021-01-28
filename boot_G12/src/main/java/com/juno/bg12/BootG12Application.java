cd377a1e104131f03e4ac494e30b3bf74ad1e0f6
Juno <juno@Juno-MacBookPro.local>
2021-01-28 15:18:45 +0900

    bg12 init

---
+++ boot_G12/src/main/java/com/juno/bg12/BootG12Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG12Application.class, args);
+	}
+
+}
---
+++ boot_G12/src/test/java/com/juno/bg12/BootG12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.bg12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootG12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
