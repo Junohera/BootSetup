4dc969d8fc45082d300fc2d65ef485895b38cb15
Juno <juno@Juno-MacBookPro.local>
2021-01-28 09:51:31 +0900

    bg08 init

---
+++ boot_G08/src/main/java/com/juno/bg08/BootG08Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG08Application.class, args);
+	}
+
+}
---
+++ boot_G08/src/test/java/com/juno/bg08/BootG08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.bg08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootG08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
