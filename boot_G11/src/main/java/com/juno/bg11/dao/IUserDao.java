55b62510b539ef35d8ea16424da11fa4a3d890d2
Juno <juno@Juno-MacBookPro.local>
2021-01-28 14:39:34 +0900

    @Mapper

---
+++ boot_G11/src/main/java/com/juno/bg11/dao/IUserDao.java
@@ -0,0 +1,12 @@
+package com.juno.bg11.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.juno.bg11.dto.User;
+
+@Mapper
+public interface IUserDao {
+	public List<User> list();
+}
