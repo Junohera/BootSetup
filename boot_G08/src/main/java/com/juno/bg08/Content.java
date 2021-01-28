754dbecb033b87accbeca0e45f71f0638a07183e
Juno <juno@Juno-MacBookPro.local>
2021-01-28 09:59:08 +0900

    @NotEmpty

--- boot_G08/src/main/java/com/juno/bg08/Content.java
+++ boot_G08/src/main/java/com/juno/bg08/Content.java
@@ -1,5 +1,6 @@
 package com.juno.bg08;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
@@ -8,7 +9,9 @@ import lombok.Data;
 public class Content {
 	private int id;
 	@NotNull(message="Writer is Null")
+	@NotEmpty(message="Writer is Empty")
 	private String writer;
 	@NotNull(message="Content is Null")
+	@NotEmpty(message="Content is Empty")
 	private String content;
 }
