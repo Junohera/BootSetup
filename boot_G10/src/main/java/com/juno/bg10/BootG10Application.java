b15b70711bfa2180f89586224f5a4672f21f9996
Juno <juno@Juno-MacBookPro.local>
2021-01-28 11:47:44 +0900

    bg10 init

---
+++ boot_G10/src/main/java/com/juno/bg10/BootG10Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG10Application.class, args);
+	}
+
+}
