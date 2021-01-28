607de96acc4723db16ed7582d1d64d44689bbda5
Juno <juno@Juno-MacBookPro.local>
2021-01-28 10:14:09 +0900

    에러 핸들링

--- boot_G08/src/main/java/com/juno/bg08/ValidController.java
+++ boot_G08/src/main/java/com/juno/bg08/ValidController.java
@@ -23,8 +23,14 @@ public class ValidController {
 			, BindingResult result) {
 		String page = "createDonePage";
 		
-		if (result.hasErrors()) {
-			
+		if (result.hasErrors()) { // 어느 멤버변수인지 모르지만, 에러내용이 존재한다면
+			if (result.getFieldError("writer") != null) { // writer 멤버변수에 대한 에러내용이 존재한다면
+				System.out.println("1: " + result.getFieldError("writer").getDefaultMessage());
+			}
+			if (result.getFieldError("content") != null) { // writer 멤버변수에 대한 에러내용이 존재한다면
+				System.out.println("2: " + result.getFieldError("content").getDefaultMessage());
+			}
+			page = "createPage";
 		}
 		
 		return page;
