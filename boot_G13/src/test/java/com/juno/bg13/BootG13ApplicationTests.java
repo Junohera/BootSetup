095a17ac3937e7c89ec5ebd7abceeb917389973a
Juno <juno@Juno-MacBookPro.local>
2021-01-29 09:40:54 +0900

    bg13 init

---
+++ boot_G13/src/test/java/com/juno/bg13/BootG13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.bg13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootG13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
