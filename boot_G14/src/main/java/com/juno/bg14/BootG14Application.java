12ee6909d17ffcda726d282d37530ca83f49c0fe
Juno <juno@Juno-MacBookPro.local>
2021-02-01 13:26:35 +0900

    bg14 init

---
+++ boot_G14/src/main/java/com/juno/bg14/BootG14Application.java
@@ -0,0 +1,13 @@
+package com.juno.bg14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG14Application.class, args);
+	}
+
+}
