7b103691fbd3181f937c7efc2334d781ea89db41
Juno <juno@Juno-MacBookPro.local>
2021-02-01 16:43:16 +0900

    mg01 init

---
+++ boot_M01/src/main/java/com/juno/bm01/BootM01Application.java
@@ -0,0 +1,13 @@
+package com.juno.bm01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootM01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootM01Application.class, args);
+	}
+
+}
