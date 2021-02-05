28172e47642874b106a5d4c3b2c993fc6e83bbb0
juno <selectjuno@gmail.com>
2021-02-05 09:49:36 +0900

    bg15 init 4transaction

---
+++ Boot_G15_Transaction/src/main/java/com/juno/bg15/BootG15TransactionApplication.java
@@ -0,0 +1,13 @@
+package com.juno.bg15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootG15TransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootG15TransactionApplication.class, args);
+	}
+
+}
